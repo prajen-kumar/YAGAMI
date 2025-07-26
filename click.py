d2d82517b48c7ceb8995448abef8fc4df1634425
prajen-kumar <kumar425879@gmail.com>
2025-07-26 08:01:57 +0530

    Update click.py

--- click.py
+++ click.py
@@ -1,14 +1,15 @@
+#click to count
 from tkinter import *
 count=0
 def inc():
     global count
     count+=1
-    label.config(text=f"Count:{count}")
+    label.config(text=f"COUNT:{count}")
 win=Tk()
 label=Label(win,text="hello",font=("stencil",50))
 label.pack()
 
-lb=Button(win,text="click",command=inc)
+lb=Button(win,text="click",font=("Stencil",20)command=inc)
 lb.pack()
 
 win.mainloop()
